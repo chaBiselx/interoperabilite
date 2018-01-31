ddcaf8bc37765878fad39868298518958fa04ef8
valentin <valentin.escamilla@outlook.fr>
2018-01-31 14:40:14 +0100

    Add xsltprocessor

--- index.php
+++ index.php
@@ -20,9 +20,18 @@ $lat = $positionXml->Latitude;
 $long = $positionXml->Longitude;
 // var_dump($positionXml);
 
+//XSLTprocessor
+$xslt = new XSLTProcessor(); 
+$XSL = new DOMDocument(); 
+$XSL->load( 'meteo.xslt' );
+$xslt->importStylesheet( $XSL ); 
+
 $meteoXml = getXml("http://www.infoclimat.fr/public-api/gfs/xml?_ll=" . $lat . "," . $lng . "&_auth=ARsDFFIsBCZRfFtsD3lSe1Q8ADUPeVRzBHgFZgtuAH1UMQNgUTNcPlU5VClSfVZkUn8AYVxmVW0Eb1I2WylSLgFgA25SNwRuUT1bPw83UnlUeAB9DzFUcwR4BWMLYwBhVCkDb1EzXCBVOFQoUmNWZlJnAH9cfFVsBGRSPVs1UjEBZwNkUjIEYVE6WyYPIFJjVGUAZg9mVD4EbwVhCzMAMFQzA2JRMlw5VThUKFJiVmtSZQBpXGtVbwRlUjVbKVIuARsDFFIsBCZRfFtsD3lSe1QyAD4PZA%3D%3D&_c=19f3aa7d766b6ba91191c8be71dd1ab2");
 
 // var_dump($meteoXml);
+
+//apply xslt template
+$xslt->transformToXML( $meteoXml );
 
 function getXml($url) {
   $file = file_get_contents($url);
